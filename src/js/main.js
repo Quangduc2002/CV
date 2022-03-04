6604c0d798a0b395ce836cce3c9c282f40af54e4
quangduc2002 <phamquangduc110@gmail.com>
2022-03-04 11:42:47 +0700

    add file js

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,23 @@ function hide(){
 bars.addEventListener('click', show)
 xmark.addEventListener('click', hide)
 model.addEventListener('click', hide)
+
+// click vào menu chuyển sang mục khác
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const tabs = $$(".tab-item");
+const panes = $$(".tab-pane");
+
+
+tabs.forEach((tab, index) => {
+  const pane = panes[index];
+
+  tab.onclick = function () {
+    $(".tab-item.active").classList.remove("active");
+    $(".tab-pane.active").classList.remove("active");
+
+    this.classList.add("active");
+    pane.classList.add("active");
+  };
+});
